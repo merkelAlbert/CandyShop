19cba7e7bb56d538ab3e0e9e57acab9e93debd06
merkelAlbert <merkel.albert1997@gmail.com>
2019-02-15 13:21:05 +0300

    project base

---
+++ Console/ConsoleCandyShop/ConsoleCandyShop/ConsoleCandyShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCandyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Candy Shop!");
+        }
+    }
+}
